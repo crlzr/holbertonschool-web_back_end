df611641e5af139e1c5da5d159d20c000e689baa
Carole <c.zenruffinen@hotmail.com>
2024-07-11 11:16:41 +1000

    checks in constructor

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,57 +1,51 @@
-
-  class HolbertonCourse {
-    constructor(name, length, students) {
-      this._name = name;
-      this._length = length;
-      this._students = students;
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if ((typeof name) !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if ((typeof length) !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students)) {
+      throw new TypeError('Not an array');
     }
 
-    // Getter for name
-    get name() {
-      return this._name;
-    }
-
-    // Setter for name with type check
-    set name(newName) {
-      if (typeof newName !== 'string') {
-        throw new TypeError('Name must be a string');
-      }
-      this._name = newName;
-    }
-
-    // Getter for length
-    get length() {
-      return this._length;
-    }
-
-    // Setter for length with type check
-    set length(newLength) {
-      if (typeof newLength !== 'number') {
-        throw new TypeError('Length must be a number');
-      }
-      this._length = newLength;
-    }
-
-    // Getter for students
-    get students() {
-      return this._students;
-    }
-
-    // Setter for students with type check
-    set students(newStudents) {
-      if (!Array.isArray(newStudents)) {
-        throw new TypeError('Students must be an array of strings');
-      }
-
-      for (let i = 0; i < newStudents.length; i++) {
-        if (typeof newStudents[i] !== 'string') {
-          throw new TypeError('Students must be an array of strings');
-        }
-      }
-
-      this._students = newStudents;
-    }
-
+    // Use the setters instead of assigning directly to the priv vars
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
-  export default HolbertonCourse;
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if ((typeof newName) !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLen) {
+    if ((typeof newLen) !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = Number(newLen);
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) {
+      throw new TypeError('Not an array');
+    }
+    this._students = newStudents;
+  }
+}
