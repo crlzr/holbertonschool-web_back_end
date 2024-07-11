f7a1336f49c502ab638f73179f638d5753e4b8b3
Carole <c.zenruffinen@hotmail.com>
2024-07-11 13:31:49 +1000

    task4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getter and setter amount
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  // Getter and setter currency
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  // Method displayFullPrice
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} ${this._currency.code}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
