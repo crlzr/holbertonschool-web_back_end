63d1f1c1eb4080b4a266fcbd14447fa5904e83cf
Carole <c.zenruffinen@hotmail.com>
2024-07-12 11:21:22 +1000

    changed code

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,23 +1,14 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
-  }
-
-  // Getter and setter for sqft attribute
-  get sqft() {
-    return this._sqft;
-  }
-
-  set sqft(newSqft) {
-    this._sqft = newSqft;
-  }
-
-  // Abstract method for evacuationWarningMessage
-  evacuationWarningMessage() {
     if (this.constructor.name !== 'Building') {
       if (typeof this.evacuationWarningMessage !== 'function') {
         throw Error('Class extending Building must override evacuationWarningMessage');
       }
     }
+  }
+
+  get sqft() {
+    return this._sqft;
   }
 }
