9f4c3e77f0ba9837634300c97cc6a372251072c7
Carole <c.zenruffinen@hotmail.com>
2024-07-12 14:56:49 +1000

    task1

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,14 @@
+export default function getListStudentIds(array) {
+  let array2 = [];
+
+  // check if the argument passed in is NOT an array
+  if (!Array.isArray(array)) {
+  // return an empty array
+    return [];
+  // if the argument passed in is an array use map function
+  } else {
+  // Iterate over each element of the array and extract the id
+    array2 = array.map((elementofarray) => elementofarray.id);
+    return array2;
+  }
+}
