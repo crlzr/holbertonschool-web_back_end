a560bc54397844ca12167ec55b5f2bac8df3d30c
Carole <c.zenruffinen@hotmail.com>
2024-07-12 14:12:24 +1000

    task0

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+export default function getListStudents() {
+  // Define an array to store objects
+  const array = [
+
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+
+  return array;
+}
