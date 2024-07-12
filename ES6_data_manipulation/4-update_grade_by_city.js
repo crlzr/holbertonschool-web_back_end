1325d4d8503e7bc7f18a63c544c84e787bef9d64
Carole <c.zenruffinen@hotmail.com>
2024-07-12 16:34:24 +1000

    task4

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,20 @@
+// return an array of students for a specific city with their new grade
+
+export default function updateStudentGradeByCity(array, city, newGrades) {
+  // filter students by city
+  const filteredStudents = array.filter(({location}) => location === city);
+  // Iterate (map) over the filtered students to update their grades
+  const updatedStudents = filteredStudents.map(student => {
+    const studentGrades = newGrades.filter((grade) => (grade.studentID === student.id));
+    const newStudent = student;
+    // check if student has a grade, if not it should be N/A
+    if (studentGrades.length > 0) {
+      newStudent.grade = studentGrades[0].grade;
+    } else {
+      newStudent.grade = 'N/A';
+    }
+    return newStudent;
+  });
+
+  return updatedStudents;
+}
