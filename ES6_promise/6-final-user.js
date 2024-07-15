4926eb2c976be2f15a8bff45b83d9ee212d2f780
Carole <c.zenruffinen@hotmail.com>
2024-07-15 15:07:07 +1000

    task6

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,19 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const user = signUpUser(firstName, lastName)
+    .then((value) => ({
+      status: 'fulfilled',
+      value,
+    }));
+
+  const photo = uploadPhoto(fileName)
+    .catch((e) => ({
+      status: 'rejected',
+      value: e.toString(),
+    }));
+
+  const results = await Promise.all([user, photo]);
+  return results;
+}
