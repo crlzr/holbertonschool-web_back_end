49d12d5d8774b0118eb0abc3687e01ee98c72b58
Carole <c.zenruffinen@hotmail.com>
2024-07-15 15:46:15 +1000

    changed the code

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,10 +1,7 @@
 export default function divideFunction(numerator, denominator) {
-  const executorFunction = (resolve, reject) => {
-    if (denominator !== 0) {
-      return resolve(numerator / denominator);
-    }
-    return reject(Error('cannot divide by 0'));
-  };
-  const myPromise = new Promise(executorFunction);
-  return myPromise;
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  }
+
+  return numerator / denominator;
 }
