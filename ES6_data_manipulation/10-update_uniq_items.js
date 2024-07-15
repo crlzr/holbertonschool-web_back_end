c5440fbd7b90529ffaf3db84fdef2b3c19ed5129
Carole <c.zenruffinen@hotmail.com>
2024-07-15 11:14:45 +1000

    task10

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,13 @@
+/* Create a function named updateUniqueItems that returns an
+updated map for all items with initial quantity at 1. */
+
+export default function updateUniqueItems(map) {
+  // iterate over key-value pairs
+  for (const [key, value] of map.entries()) {
+    // check if value is 1
+    if (value === 1) {
+      // update value to 100
+      map.set(key, 100);
+    }
+  }
+}
