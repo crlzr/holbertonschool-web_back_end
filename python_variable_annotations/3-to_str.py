0fc99f8aeb216faed4d679d8f9e52df5fbc316e8
Carole <c.zenruffinen@hotmail.com>
2024-08-12 14:51:46 +1000

    task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+''' Module function that takes a float n and returns the string rep'''
+
+
+def to_str(n: float) -> str:
+    '''
+    function 'to_str'
+    Argument: float n
+    Return: string rep of the float
+    '''
+    return str(n)
