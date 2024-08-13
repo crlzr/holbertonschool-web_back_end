b314636e5045ffef0b340c1f9bb9020747b69068
Carole <c.zenruffinen@hotmail.com>
2024-08-13 16:00:49 +1000

    deleted the word async from function definition

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -7,7 +7,7 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int = 10) -> float:
+def measure_time(n: int, max_delay: int = 10) -> float:
     '''
     Function: 'measure_time'
     Arguments: two int's
