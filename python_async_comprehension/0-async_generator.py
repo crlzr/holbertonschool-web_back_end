f95edc6707b3f1afc41f520b36eaf698c97c4d87
Carole <c.zenruffinen@hotmail.com>
2024-08-19 11:18:18 +1000

    docstrings

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-
+''' Module that loops 10 times, generates random number, async'''
 from random import random
 from typing import Generator
 import asyncio
 
 
 async def async_generator() -> Generator[float, None, None]:
+    '''function: async_generator, arguments:none,
+    return: random numbers bw 1-10 '''
     for i in range(10):
         await asyncio.sleep(1)
         yield random() * 10
