3d95c513f07b7502f3a8c76b72a4744036fb51a6
Carole <c.zenruffinen@hotmail.com>
2024-08-19 14:25:23 +1000

    task2

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+''' find correct indices to paginate dataset or return empty list '''
+import csv
+import math
+from typing import List, Dict
+
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''
+        Args: page = 1, page_size = 10
+        Return: appropriate page of the dataset
+        If input arguments are out of range, return empty list
+        '''
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page > 0
+
+        data = self.dataset()
+        start_index, end_index = index_range(page, page_size)
+
+        if start_index >= len(data):
+            return []
+        else:
+            return data[start_index:end_index]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        '''
+        Returns a dictionary of hypermedia key-value pairs
+        '''
+        # The length of the returned dataset page
+        start_index, end_index = index_range(page, page_size)
+
+        # next_page: number of the next page, None if no next page
+        next_page = None
+        if len(self.dataset()) > end_index:
+            next_page = page + 1
+
+        # number of the previous page, None if no previous page
+        prev_page = None
+        if len(self.dataset()) > 1:
+            prev_page = page - 1
+
+        # total_pages: the total number of pages in the dataset as an integer
+        total_pages = int(len(self.dataset()) / 10)
+        if (page_size > 0):
+            total_pages = int(len(self.dataset()) / page_size)
+
+        hyper_dict = {
+            "page_size": len(self.get_page(page, page_size)),
+            "page": page,
+            "data": self.get_page(page, page_size),
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
+        }
+
+        return hyper_dict
