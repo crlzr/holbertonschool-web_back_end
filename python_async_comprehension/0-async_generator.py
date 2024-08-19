20b6bc9a10e143e97ec519c3b4f907c86bc04df2
Carole <c.zenruffinen@hotmail.com>
2024-08-19 11:03:28 +1000

    task0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,9 @@
+from random import random
+from typing import Generator
+import asyncio
+
+
+async def async_generator():
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random() * 10
