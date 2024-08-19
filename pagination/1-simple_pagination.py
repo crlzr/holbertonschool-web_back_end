7a473d8e4513bb5925dddc15c910710b6cd2c57a
Carole <c.zenruffinen@hotmail.com>
2024-08-19 14:03:42 +1000

    added docstring

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
+''' find correct indices to paginate dataset or return empty list '''
 import csv
 import math
 from typing import List
-
 index_range = __import__('0-simple_helper_function').index_range
 
 
@@ -26,6 +26,11 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''
+        Args: page = 1, page_size = 10
+        Return: appropriate page of the dataset
+        If input arguments are out of range, return empty list
+        '''
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page > 0
 
