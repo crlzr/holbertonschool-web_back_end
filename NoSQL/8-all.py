09b537e3690121ba97d3f506d35ca2ff67950723
Carole <c.zenruffinen@hotmail.com>
2024-08-20 15:52:31 +1000

    changed code

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -4,8 +4,8 @@ from pymongo import MongoClient
 
 def list_all(mongo_collection):
     ''' lists all documents in collection '''
-    cursor = mongo_collection
+    documents = []
 
     for x in mongo_collection.find():
-        return cursor
-    return []
+        documents.append(x)
+    return documents
