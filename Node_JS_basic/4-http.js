6fbd33a09ec3ad4a09e14d0325c1a9c766595103
Carole <c.zenruffinen@hotmail.com>
2024-09-04 16:23:48 +1000

    making the linter happy

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -4,12 +4,12 @@ const host = 'localhost';
 const port = 1245;
 
 function requestListener(req, res) {
-    res.writeHead(200);
-    res.end("Hello Holberton School!");
-};
+  res.writeHead(200);
+  res.end('Hello Holberton School!');
+}
 
 const app = http.createServer(requestListener);
 app.listen(port, host, () => {
-})
+});
 
 module.exports = requestListener;
