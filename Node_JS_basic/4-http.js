df910ca75f5cc8c05cfe32dfc3801c67c83181e4
Carole <c.zenruffinen@hotmail.com>
2024-09-04 15:23:10 +1000

    changed code

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,7 +1,10 @@
-var app = require('http');
+const http = require('http');
 
-//create a server object:
-app.createServer(function (req, res) {
-  res.write('Hello Holberton School!'); //write a response to the client
-  res.end(); //end the response
-}).listen(1245); //the server object listens on port 1245
+const app = http.createServer((req, res) => {
+  res.end('Hello Holberton School!!');
+});
+
+app.listen(1245, () => {
+  const { address, port } = app.address();
+  console.log(`Server is listening on: http://${address}:${port}`);
+})
