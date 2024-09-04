c58173b911881b406962efc989ba442709581dde
Carole <c.zenruffinen@hotmail.com>
2024-09-05 09:57:13 +1000

    trying things

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
-// Listen for user input
-process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    process.stdout.write(`Your name is: ${name}\n`);
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
-});
+  readline.question('Welcome to Holberton School, what is your name?\n', name => {
+    console.log(`Your name is: ${name}!`);
+    readline.close('This important software is now closing');
+  });
 
 
