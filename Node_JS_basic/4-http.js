6c0248652caeec9068df9d01970aed10ae70e1f3
Carole <c.zenruffinen@hotmail.com>
2024-09-04 16:04:09 +1000

    4-http.js

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,10 +1,15 @@
 const http = require('http');
 
+// Create an HTTP server and handle requests
 const app = http.createServer((req, res) => {
-  res.end('Hello Holberton School!!');
+  res.statusCode = 200; // Set the status code to 200 (OK)
+  res.setHeader('Content-Type', 'text/plain'); // Set the content type to plain text
+  res.end('Hello, Holberton School!!\n'); // Send a response
 });
 
-server.listen(1245, () => {
-  const { address, port } = app.address();
-  console.log(`Server is listening on: http://${address}:${port}`);
-})
+// Start the server and listen on port 1245
+app.listen(1245, () => {
+  // Retrieve the server address and log it
+  const addressInfo = app.address();
+  console.log(`Server is listening on: http://localhost:${addressInfo.port}`);
+});
